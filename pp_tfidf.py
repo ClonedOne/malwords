4f0b6b6953dfe8b28f09c64fb22cb5f84eb4c445
ClonedOne <severi.giorgio@gmail.com>
2017-05-09 19:33:20 +0200

    Removing words found in base (clean) execution

--- pp_tfidf.py
+++ pp_tfidf.py
@@ -1,12 +1,13 @@
 from multiprocessing import Pool
 from collections import Counter
+from workers import wk_tfidf
 from utilities import utils
 import json
-import math
 import os
 
 dir_malwords = ''
 dir_store = ''
+dir_base = ''
 core_num = 1
 
 
@@ -17,22 +18,24 @@ def get_tf_idf():
     :return: 
     """
 
-    global dir_malwords, dir_store, core_num
+    global dir_malwords, dir_store, dir_base, core_num
 
     config = json.load(open('config.json'))
     dir_malwords = config['dir_mini']
     dir_store = config['dir_store']
     core_num = config['core_num']
+    dir_base = config['dir_base']
 
     total_documents = float(len(os.listdir(dir_malwords)))
 
     print('Acquiring document frequencies')
     file_name_lists = utils.divide_workload(sorted(os.listdir(dir_malwords)), core_num)
-    formatted_input = utils.format_worker_input(core_num, file_name_lists, (total_documents, ))
+    formatted_input = utils.format_worker_input(core_num, file_name_lists, (dir_malwords,))
     pool = Pool(processes=core_num)
-    results = pool.map(compute_df, formatted_input)
+    results = pool.map(wk_tfidf.compute_df, formatted_input)
     pool.close()
     pool.join()
+
     dfs = Counter()
     for sub_dfs in results:
         dfs += sub_dfs
@@ -42,34 +45,12 @@ def get_tf_idf():
     remove_useless_words(dfs, total_documents, 0.5)
 
     print('Computing Tf-Idf values')
-    formatted_input = utils.format_worker_input(core_num, file_name_lists, (dfs, total_documents))
+    formatted_input = utils.format_worker_input(core_num, file_name_lists,
+                                                (dfs, total_documents, dir_malwords, dir_store))
     pool = Pool(processes=core_num)
-    pool.map(compute_tf_idf, formatted_input)
+    pool.map(wk_tfidf.compute_tf_idf, formatted_input)
     pool.close()
     pool.join()
-
-
-def compute_df(data_pack):
-    """
-    Scans the bag-of-words documents and computes the document frequency of each word.
-
-    :return: Counter containing the document frequency of each word
-    """
-
-    file_name_list = data_pack[1]
-    dfs = Counter()
-
-    for sample in sorted(file_name_list):
-
-        with open(os.path.join(dir_malwords, sample), 'rb') as words_file:
-
-            for line in words_file:
-                line = line.strip().split()
-
-                word = line[0].decode('utf-8')
-                dfs[word] += 1
-
-    return dfs
 
 
 def remove_useless_words(dfs, total_documents, filter_factor):
@@ -87,11 +68,20 @@ def remove_useless_words(dfs, total_documents, filter_factor):
 
     to_remove = set()
     threshold = filter_factor * total_documents
+
     singleton_words = 0
     frequent_words = 0
+    in_base = 0
 
     print('Initial features number:', len(dfs))
     print('Document frequency threshold:', threshold)
+
+    base_words = set()
+    base_file = os.path.join(dir_base, 'base.txt')
+    if os.path.isfile(base_file):
+        with open(base_file, 'rb') as base_in:
+            for line in base_in:
+                base_words.add(line.strip().split()[0].decode('utf-8'))
 
     for word in dfs:
         if dfs[word] == 1:
@@ -100,6 +90,9 @@ def remove_useless_words(dfs, total_documents, filter_factor):
         if dfs[word] >= threshold:
             to_remove.add(word)
             frequent_words += 1
+        if word in base_words:
+            to_remove.add(word)
+            in_base += 1
 
     for word in to_remove:
         dfs.pop(word, None)
@@ -107,62 +100,12 @@ def remove_useless_words(dfs, total_documents, filter_factor):
     print('Features number:', len(dfs))
     print('Words appearing only once:', singleton_words)
     print('Words with over threshold frequency:', frequent_words)
+    print('Words appearing in base:', in_base)
 
     # Create a dictionary mapping each (sorted) word with a numerical index
     words = dict(zip(sorted(list(dfs.keys())), list(range(len(dfs)))))
 
     json.dump(words, open('data/words.json', 'w'), indent=2)
-
-
-def compute_tf_idf(data_pack):
-    """
-    Scans the bag-of-words documents and computes the term frequency value of each word.
-    During the process computes the document frequency of each word.
-
-    :return:  
-    """
-
-    file_name_list = data_pack[1]
-    dfs = data_pack[2]
-    total_documents = data_pack[3]
-
-    norm_factor = 0.4
-
-    for sample in sorted(file_name_list):
-        words = Counter()
-        total_words = 0
-        document_length = 0
-
-        # Scan once the bag of words files and memorize the words in a temporary per-file dictionary
-        with open(os.path.join(dir_malwords, sample), 'rb') as words_file:
-            for line in words_file:
-                line = line.strip().split()
-
-                word = line[0].decode('utf-8')
-                count = int(line[1])
-
-                # avoid excluded words
-                if word not in dfs:
-                    continue
-
-                words[word] = count
-                document_length += count
-                total_words += 1
-
-        uuid = sample[:-7]
-        tf_idf = {}
-
-        # find the most frequent word
-        most_freq = max(list(words.values()))
-
-        # Compute the term frequency of a word using double normalization
-        for word in words:
-            tf = norm_factor + ((1 - norm_factor) * (float(words[word]) / float(most_freq)))
-            idf = math.log(total_documents / float(dfs[word]))
-            tf_idf[word] = tf * idf
-
-        json.dump(tf_idf, open(os.path.join(dir_store, uuid), "w"), indent=2)
-        # print(uuid, total_words, document_length)
 
 
 if __name__ == '__main__':
---
+++ workers/wk_tfidf.py
@@ -0,0 +1,81 @@
+from collections import Counter
+import json
+import math
+import os
+
+
+def compute_tf_idf(data_pack):
+    """
+    Scans the bag-of-words documents and computes the term frequency value of each word.
+    During the process computes the document frequency of each word.
+
+    :return:  
+    """
+
+    file_name_list = data_pack[1]
+    dfs = data_pack[2]
+    total_documents = data_pack[3]
+    dir_malwords = data_pack[4]
+    dir_store = data_pack[5]
+
+    norm_factor = 0.4
+
+    for sample in sorted(file_name_list):
+        words = Counter()
+        total_words = 0
+        document_length = 0
+
+        # Scan once the bag of words files and memorize the words in a temporary per-file dictionary
+        with open(os.path.join(dir_malwords, sample), 'rb') as words_file:
+            for line in words_file:
+                line = line.strip().split()
+
+                word = line[0].decode('utf-8')
+                count = int(line[1])
+
+                # avoid excluded words
+                if word not in dfs:
+                    continue
+
+                words[word] = count
+                document_length += count
+                total_words += 1
+
+        uuid = sample[:-7]
+        tf_idf = {}
+
+        # find the most frequent word
+        most_freq = max(list(words.values()))
+
+        # Compute the term frequency of a word using double normalization
+        for word in words:
+            tf = norm_factor + ((1 - norm_factor) * (float(words[word]) / float(most_freq)))
+            idf = math.log(total_documents / float(dfs[word]))
+            tf_idf[word] = tf * idf
+
+        json.dump(tf_idf, open(os.path.join(dir_store, uuid), "w"), indent=2)
+        # print(uuid, total_words, document_length)
+
+
+def compute_df(data_pack):
+    """
+    Scans the bag-of-words documents and computes the document frequency of each word.
+
+    :return: Counter containing the document frequency of each word
+    """
+
+    file_name_list = data_pack[1]
+    dir_malwords = data_pack[2]
+    dfs = Counter()
+
+    for sample in sorted(file_name_list):
+
+        with open(os.path.join(dir_malwords, sample), 'rb') as words_file:
+
+            for line in words_file:
+                line = line.strip().split()
+
+                word = line[0].decode('utf-8')
+                dfs[word] += 1
+
+    return dfs
